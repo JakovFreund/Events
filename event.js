6c2da19aeafa540210871177a569424ac88f7631
JakovFreund <jakovfreund@gmail.com>
2017-03-16 06:12:37 -0200

    Add files via upload

---
+++ event.js
@@ -0,0 +1,8 @@
+document.getElementById("mojGumb").addEventListener("mouseover", funkc);
+
+var moj_string="lala";
+
+function funkc(){
+    alert("OwO");
+    document.getElementById("mojGumb").innerHTML=moj_string;
+}
---
+++ event2.js
@@ -0,0 +1,14 @@
+
+document.getElementById("abc").addEventListener("keydown", funkc);
+document.getElementById("def").addEventListener("keyup", funkc);
+document.getElementById("ghi").addEventListener("keypress", funkc);
+
+function funkc(){
+    alert("sad");
+}
+
+document.getElementById("sl").addEventListener("load", funkci);
+
+function funkci(){
+    alert("slika je ucitana");
+}
